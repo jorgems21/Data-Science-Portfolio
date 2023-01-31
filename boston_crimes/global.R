d0a9dd74fe6f30b2c8b55e7078fda5aeed44aaa7
jorgems21 <montoya.j@northeastern.edu>
2023-01-30 23:14:13 -0600

    Updates: 1) continue working on the wireframe of the app 2) Cleaned the data more, mapped the district codes to names and dropped unecessary columns

--- boston_crimes/global.R
+++ boston_crimes/global.R
@@ -15,7 +15,7 @@ library(dplyr)
 
 df <- Crime_Incident_Reports_August_2015_September_2020_
 # 
-str(df)
+str(clean_df)
 is.na(df)
 summary(df)
 unique(df$OFFENSE_CODE_GROUP)
@@ -29,6 +29,36 @@ clean_df$SHOOTING[is.na(clean_df$SHOOTING)]<- 0
 
 clean_df$SHOOTING[clean_df$SHOOTING=="Y"] <- 1
 
-clean_df <- clean_df %>% subset(select = -c(INCIDENT_NUMBER,REPORTING_AREA, UCR_PART))
+clean_df <- clean_df %>% subset(select = -c(INCIDENT_NUMBER,REPORTING_AREA, UCR_PART, OFFENSE_CODE))
 
 #Map 
+clean_df$OFFENSE_CODE_GROUP<- as.factor(clean_df$OFFENSE_CODE_GROUP)
+
+clean_df$DISTRICT <- gsub('A1', 'Downtown', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('A15', 'Charlestown', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('A7', 'East Boston', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('B2', 'Roxbury', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('B3', 'Mattapan', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('C6', 'South Boston', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('C11', 'Dorchester',clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('D4', 'South End', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('D14', 'Brighton', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('E18', 'Hyde Park', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('E13', 'Jamaica Plain', clean_df$DISTRICT)
+clean_df$DISTRICT <- gsub('E5', 'West Roxbury', clean_df$DISTRICT)
+
+districts <- unique(clean_df$DISTRICT)
+
+clean_df %>%
+  group_by(OFFENSE_CODE_GROUP) %>%
+  summarise(counts = n()) %>%
+  arrange(desc(counts))
+
+# d <- filter(myDf, OFFENSE_CODE_GROUP == "Robbery" | 
+#               OFFENSE_CODE_GROUP == "Homicide" | 
+#               OFFENSE_CODE_GROUP == "Residential Burglary" |
+#               OFFENSE_CODE_GROUP == "Aggravated Assault" |
+#               OFFENSE_CODE_GROUP == "Simple Assault" |
+#               OFFENSE_CODE_GROUP == "Other Burglary" |
+#               OFFENSE_CODE_GROUP == "HOME INVASION" |
+ #             OFFENSE_CODE_GROUP == "Burglary - No Property Taken")
--- boston_crimes/ui.R
+++ boston_crimes/ui.R
@@ -11,48 +11,20 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
-  navbarPage(title = "Title",
-             tabItem(
-               tabName = "main1",
-               fluidRow(
-                 column(5,
-                        box(width = 15,
-                            title = "Number and Type of Crime by District",
-                            color = "green", ribbon = TRUE, title_side = "top right",
-                            height = "1000",
-                            checkboxGroupInput("checkGroup", 
-                                               h3("Checkbox group"),
-                                               #choices = offenseCodeGrp,
-                                               #selected = 1
-                                               ),
+  navbarPage(title = "Crime in Boston",id= "main",
+             tabPanel("General",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("variable", "Districts:", choices = sort(districts), multiple = TRUE)
+
                         ),
-                 ),
-                 # column(7,
-                 #        box(width = 20,
-                 #            title = "Rate of Incident over District", ribbon = TRUE, title_side = "top right",
-                 #            column(width = 12, height = "auto", plotOutput("plot"))
-                 #        ),
-                 # ),
-               ),
+                        mainPanel(DT::dataTableOutput("bus_table"))
+                      )),
+             navbarMenu("Map",
+                        tabPanel("Summary"),
+                        "----",
+                        "Section header",
+                        tabPanel("Table")
              )
   )
-
-    # # Application title
-    # titlePanel("Old Faithful Geyser Data"),
-    # 
-    # # Sidebar with a slider input for number of bins
-    # sidebarLayout(
-    #     sidebarPanel(
-    #         sliderInput("bins",
-    #                     "Number of bins:",
-    #                     min = 1,
-    #                     max = 50,
-    #                     value = 30)
-    #     ),
-    # 
-    #     # Show a plot of the generated distribution
-    #     mainPanel(
-    #         plotOutput("distPlot")
-    #     )
-    # )
 )
